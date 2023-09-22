9ca5ee11c9f790e591fc543ca64a09802417e771
Lene <lenecarpertunnel@gmail.com>
2023-09-22 17:17:04 +0200

    Portfolio layout, placeholder projects

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// Typing effect
+
+// Wait for the document to load
+document.addEventListener("DOMContentLoaded", function ()
+{
+    // Get the ID of the portfolio header text
+    const textElement = document.getElementById("type-smooth");
+    // Text to add into the header
+    const textToType = "Milan van Houts Portfolio";
+    // Current character position
+    let charIndex = 0;
+
+    // Function to type the characters out
+    function typeCharacter()
+    {
+        // If the character position is less than the entire word length, run below code
+        if (charIndex < textToType.length)
+        {
+            // Save the letters into a variable
+            const char = textToType.charAt(charIndex);
+            // Append the letters one by one into the header text element, delay by 75ms
+            textElement.innerHTML += char;
+            charIndex++;
+            setTimeout(typeCharacter, 75);
+        }
+    }
+
+    // Start the typing effect
+    typeCharacter();
+});
