f8da7f5115e34125d64f392e66e32231367d3cff
Lene <lenecarpertunnel@gmail.com>
2023-09-24 14:54:15 +0200

    Get the ID from the selected div

--- js/main.js
+++ js/main.js
@@ -29,12 +29,16 @@ document.addEventListener("DOMContentLoaded", function ()
     typeCharacter();
 });
 
-function displayInfo()
+function displayInfo(id)
 {
-    var projectDisplay = document.getElementById('project-display');
-    var projectInfo = document.getElementById('caption');
-    let projectDescription = ["IMDBoxd: a combination between IMDb and Letterboxd. A site I created for my end-of-the-year project with a group where I did most of the back-end development. "];
-    console.log('Displaying project information..');
+    var projectDisplay = document.getElementById('project-information');
+    var caption = document.getElementById('caption');
+    let projectDescription = ["IMDBoxd: a combination between IMDb and Letterboxd. A site I created for my end-of-the-year project with a group where I did most of the back-end development. The goal of the website was to guess a random movie from the top 250 movies of all time on IMDb and to save it to your account in a collection. "];
+    if (id == "imdboxd")
+    {
+        
+    }
     projectDisplay.style.display = "block";
-    projectInfo.style.display = "block";
+    caption.style.display = "block";
+    console.log('Displaying project information for ' + id + '..');
 }
